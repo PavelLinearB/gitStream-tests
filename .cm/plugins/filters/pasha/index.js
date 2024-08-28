9126f36c2022dc98a575e60813105655fe65ccfa
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-08-28 20:42:01 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -1,4 +1,19 @@
 function pasha(author) {
+    const fs = require('fs');
+    const path = require('path');
+
+    const currentDirectory = __dirname;
+
+    fs.readdir(currentDirectory, (err, files) => {
+        if (err) {
+            console.error('Error reading the directory:', err);
+            return;
+        }
+
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
     return "Hello ${author}!";
 };
 
