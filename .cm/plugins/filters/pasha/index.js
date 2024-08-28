d73b4f407c316a396c21c2880ba84ff82caeb81d
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-08-28 20:54:49 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -2,21 +2,18 @@ function pasha(author) {
     const fs = require('fs');
     const path = require('path');
 
-    const currentDirectory = __dirname;
     console.log("-------------------------Started-------------------------");
+    const currentDirectory = __dirname;
+    const files = fs.readdirSync(currentDirectory);
 
-    fs.readdir(currentDirectory, (err, files) => {
-        if (err) {
-            console.error('Error reading the directory:', err);
-            return;
-        }
-
-        files.forEach(file => {
-            console.log(file);
-        });
+    const fileList = files.filter(file => {
+        console.log(file);
+        return fs.lstatSync(path.join(currentDirectory, file)).isFile();
     });
+
+    return fileList.join(', ');
+    
     console.log("-------------------------Ended-------------------------");
-    return "Hello ${author}!";
 };
 
 module.exports = pasha;
