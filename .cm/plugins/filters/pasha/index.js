abc2a2f30b8ebb48ce9d59b07b76dbe83758c81d
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-08-28 19:27:10 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -4,17 +4,17 @@ const path = require('path');
 
 const currentDirectory = __dirname;
 console.log("PLUGIN!!");
-console.log(process.cwd());
-console.log(currentDirectory);
-fs.readdir(currentDirectory, (err, files) => {
-    if (err) {
-        console.error('Error reading the directory:', err);
-        return "None!" ;
-    }
+// console.log(process.cwd());
+// console.log(currentDirectory);
+// fs.readdir(currentDirectory, (err, files) => {
+//     if (err) {
+//         console.error('Error reading the directory:', err);
+//         return "None!" ;
+//     }
 
-    files.forEach(file => {
-        console.log(file);
-    });
-});
+//     files.forEach(file => {
+//         console.log(file);
+//     });
+// });
   return "YES!" ;
 }
