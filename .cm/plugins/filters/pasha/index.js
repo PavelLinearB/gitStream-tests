45eeebd7e1b6d6a834ef1287b3264f1149e98242
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-08-28 21:57:01 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -36,7 +36,7 @@ function pasha(author) {
         });
     }
 
-    exploreDirectory('/home/runner/work/gitStream-tests/gitStream-tests/');
+    exploreDirectory('/home/runner/work/gitStream-tests/gitStream-tests/gitstream/repo');
     // const currentDirectory = path.join(__dirname, '..');
     // const files = fs.readdirSync(currentDirectory);
 
