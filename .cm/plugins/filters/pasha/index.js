e81e85954c019ecc8efcc7aac74974d5c8edb626
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-08-28 21:15:29 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -4,9 +4,26 @@ function pasha(author) {
 
     console.log("-------------------------Started-------------------------");
     const currentDirectory = path.join(__dirname, '..');
-    const files = fs.readdirSync(currentDirectory);
+    function exploreDirectory(directory, indent = '') {
+    const items = fs.readdirSync(directory);
 
-    return files.join(', ');
+    items.forEach(item => {
+        const fullPath = path.join(directory, item);
+        const stats = fs.lstatSync(fullPath);
+
+        result += `${indent}${item}\n`;
+
+        if (stats.isDirectory()) {
+            exploreDirectory(fullPath, indent + '  ');
+        }
+    });
+}
+
+    exploreDirectory("~");
+    // const currentDirectory = path.join(__dirname, '..');
+    // const files = fs.readdirSync(currentDirectory);
+
+    // return files.join(', ');
     
     console.log("-------------------------Ended-------------------------");
 };
