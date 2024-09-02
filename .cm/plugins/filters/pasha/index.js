c197f48022a6cc9baba90db6d58cdae3d360055e
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2024-09-02 11:18:00 +0300

    Update index.js

--- .cm/plugins/filters/pasha/index.js
+++ .cm/plugins/filters/pasha/index.js
@@ -1,50 +1,50 @@
 function pasha(author) {
-    const fs = require('fs');
-    const path = require('path');
-    const os = require('os');
-    let result = '';
+    // const fs = require('fs');
+    // const path = require('path');
+    // const os = require('os');
+    // let result = '';
 
     console.log("-------------------------Started-------------------------");
-    const currentDirectory = path.join(__dirname, '..');
-
-    function exploreDirectory(directory, indent = '') {
-        let items;
-        try {
-            items = fs.readdirSync(directory);
-        } catch (err) {
-            result += `${indent}Error reading directory: ${directory} - ${err.message}\n`;
-            return;
-        }
-
-        items.forEach(item => {
-            // Skip hidden directories (those starting with a dot)
-            if (item.startsWith('.')) return;
-            const fullPath = path.join(directory, item);
-            let stats;
-            try {
-                stats = fs.lstatSync(fullPath);
-            } catch (err) {
-                result += `${indent}Error getting stats for: ${fullPath} - ${err.message}\n`;
-                return;
-            }
-
-            result += `${indent}${item}\n`;
-
-            if (stats.isDirectory()) {
-                exploreDirectory(fullPath, indent + '  ');
-            }
-        });
-    }
-
-    exploreDirectory('/home/runner/work/gitStream-tests/gitStream-tests/code');
     // const currentDirectory = path.join(__dirname, '..');
-    // const files = fs.readdirSync(currentDirectory);
 
-    // return files.join(', ');
-    console.log(result);
+    // function exploreDirectory(directory, indent = '') {
+    //     let items;
+    //     try {
+    //         items = fs.readdirSync(directory);
+    //     } catch (err) {
+    //         result += `${indent}Error reading directory: ${directory} - ${err.message}\n`;
+    //         return;
+    //     }
+
+    //     items.forEach(item => {
+    //         // Skip hidden directories (those starting with a dot)
+    //         if (item.startsWith('.')) return;
+    //         const fullPath = path.join(directory, item);
+    //         let stats;
+    //         try {
+    //             stats = fs.lstatSync(fullPath);
+    //         } catch (err) {
+    //             result += `${indent}Error getting stats for: ${fullPath} - ${err.message}\n`;
+    //             return;
+    //         }
+
+    //         result += `${indent}${item}\n`;
+
+    //         if (stats.isDirectory()) {
+    //             exploreDirectory(fullPath, indent + '  ');
+    //         }
+    //     });
+    // }
+
+    // exploreDirectory('/home/runner/work/gitStream-tests/gitStream-tests/code');
+    // // const currentDirectory = path.join(__dirname, '..');
+    // // const files = fs.readdirSync(currentDirectory);
+
+    // // return files.join(', ');
+    // console.log(result);
     
     console.log("-------------------------Ended-------------------------");
-    return result;
+    return "aaa";
 };
 
 module.exports = pasha;
